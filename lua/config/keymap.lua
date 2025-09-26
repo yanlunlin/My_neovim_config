5d21a7f1441aab46ef6efc36abcdeef19c6310b9
yenlunlin <yanlunlin@github.com>
2025-09-26 23:13:53 +0800

    key

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -9,6 +9,11 @@ vim.keymap.set("n", "<leader>p", "<CMD>NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>bd", "<CMD>BufferClose<CR>")
 vim.keymap.set("n", "<leader>bn", "<CMD>BufferNext<CR>")
 
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
 vim.keymap.set("n", "<leader>g", function()
 	require("snacks").lazygit()
 end, { desc = "Lazygit" })
