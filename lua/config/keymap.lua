3860593968268ef4dad72a199ec150dd63ba36a7
yenlunlin <yanlunlin@github.com>
2025-10-04 15:04:43 +0800

    support rust

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -15,25 +15,29 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "上視窗" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "右視窗" })
 
 vim.keymap.set("n", "<leader>g", function()
-	require("snacks").lazygit()
+  require("snacks").lazygit()
 end, { desc = "開啟 Lazygit" })
 
 vim.keymap.set("n", "<leader>r", function()
-	local filename = vim.api.nvim_buf_get_name(0)
-	local ext = filename:match("^.+(%..+)$")
-	if ext == ".c" then
-		vim.cmd([[
+  local filename = vim.api.nvim_buf_get_name(0)
+  local ext = filename:match("^.+(%..+)$")
+  if ext == ".c" then
+    vim.cmd([[
       TermExec cmd="clang % -lm"
       TermExec cmd="./a.out"
     ]])
-	elseif ext == ".cpp" then
-		vim.cmd([[
+  elseif ext == ".cpp" then
+    vim.cmd([[
       TermExec cmd="clang++ %"
       TermExec cmd="./a.out"
     ]])
-	elseif ext == ".py" then
-		vim.cmd([[
+  elseif ext == ".py" then
+    vim.cmd([[
       TermExec cmd="python3 %"
     ]])
-	end
+  elseif ext == ".rs" then
+    vim.cmd([[
+      TermExec cmd="cargo run"
+    ]])
+  end
 end, { desc = "執行程式碼" })
---
+++ lua/plugins/autotag.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
+    },
+    per_filetpe = {},
+  },
+}
